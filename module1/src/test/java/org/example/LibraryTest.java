beee53e728201f0c8f987d04552f95a5115fd1a6
v2ex <v2ex@v2ex.com>
2024-06-21 15:44:21 +0800

    testkit library example

--- module1/src/test/java/org/example/LibraryTest.java
+++ module1/src/test/java/org/example/LibraryTest.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import org.junit.Test;
+public class LibraryTest extends AbstractGreetingTest{
 
-import static org.junit.Assert.*;
-
-public class LibraryTest {
-
-    @Test
-    public void test() throws InterruptedException {
-        Library library = new Library();
-        // we block the thread for 10 seconds to simulate a long-running operation
-        Thread.sleep(60_000);
-        assertEquals("Hello world!", library.getGreeting());
+    @Override
+    IGreeting getGreetingImpl() {
+        return new Library();
     }
 
 }
